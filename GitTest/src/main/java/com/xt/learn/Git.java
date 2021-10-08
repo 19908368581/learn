861429d74cec17f7c92554fee1430ab4589a9acf
xutao <1439489579@qq.com>
2021-10-08 10:40:27 +0800

    commit 1

---
+++ GitTest/src/main/java/com/xt/learn/Git.java
@@ -0,0 +1,11 @@
+package com.xt.learn;
+/*
+@author 徐涛
+@create 2021-10-08-10:39  
+*/
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello 1");
+    }
+}
